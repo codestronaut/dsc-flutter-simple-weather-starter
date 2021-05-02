4de607676500ba25a18313b2469a120f0a9d4bb1
Aditya Rohman <hello.adityarohman@gmail.com>
2021-05-02 15:12:24 +0800

    fix: add example

--- lib/data/controllers/weather_controller.dart
+++ lib/data/controllers/weather_controller.dart
@@ -1,3 +1,22 @@
 class WeatherController {
   // TODO: Complete weather controller
 }
+
+// Example
+/*
+try {
+  NetworkHelper networkHelper = NetworkHelper(
+    url: UrlList.userData,
+    headers: {'Authorization': 'Bearer $token'},
+  );
+
+  var profileData = await networkHelper.getData();
+  profile = Profile.fromJson(profileData).obs;
+} on TimeoutException catch (_) {
+  // Implement 404 page later
+  throw ("No respond after 20 seconds");
+} catch (e) {
+  // Implement 404 page later
+  throw ("Failed to fetch API");
+}
+*/
